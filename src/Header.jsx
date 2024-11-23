31782c135b0f3a8696e937a65e676d2f262735ba
vasilexa13 <vasilexa@gmail.com>
2024-11-23 21:32:47 +0300

    created: frontend application

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+
+    return (
+        <>
+            <div>
+                <h1>TEST TASK</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
