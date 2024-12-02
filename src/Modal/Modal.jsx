b762e97c7e2d6c29d3bdbb3189119c6c28f32bd4
vasilexa13 <vasilexa@gmail.com>
2024-12-02 21:57:12 +0300

    updare : modalForm

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,53 +1,58 @@
 import "./modal.css"
+// import {Formik, Field, Form} from "formik";
 
 const Modal = ({active , setActive, children}) => {
     return(
         <div className={active ? "modal active":"modal"} onClick={() => setActive(false)}>
             <div className={active ? "modal_content active":"modal_content"} onClick={e=>e.stopPropagation()}>
-                <div>
-                    <label htmlFor='companyName'>Company Name</label>
-                    <input type='text' id="companyName"></input>
-                </div>
 
-                <div>
-                    <label htmlFor='vacancyName'>Vacancy Name</label>
-                    <input type='text' id='vacancyName'></input>
-                </div>
-
-                <div>
-                    <label htmlFor="minSallary">Minimum Salary (100-10000):</label>
-                    <input type="number" id="minSallary" name="tentacles" min="100" max="10000"/>
-                </div>
-
-                <div>
-                    <label htmlFor="maxSallary">Maximum Salary (100-10000):</label>
-                    <input type="number" id="maxSallary" name="tentacles" min="100" max="10000"/>
-                </div>
-
-                <div className='reqStatus'>
-                    <div>
-                        <h5>Request Status</h5>
-                    </div>
-                    <div>
-                        <select>
-                            <option>false</option>
-                            <option>true</option>
-                        </select>
+                <form>
+                    <div className='centred'>
+                        <label htmlFor='companyName'>Company Name</label>
+                        <input type='text' id="companyName"></input>
                     </div>
 
-                </div>
+                    <div className='centred'>
+                        <label htmlFor='vacancyName'>Vacancy Name</label>
+                        <input type='text' id='vacancyName' placeholder="Junior Full Stack developer"></input>
+                    </div>
 
-                <div>
-                    <label htmlFor="note">note</label>
-                    <textarea id="note" name="note" rows="2" cols="22" value='type some note here!!!'>
+                    <div className='centred'>
+                        <label htmlFor="minSallary">Minimum Salary (100-10000):</label>
+                        <input type="number" id="minSallary" name="tentacles" min="100" max="10000"/>
+                    </div>
+
+                    <div className='centred'>
+                        <label htmlFor="maxSallary">Maximum Salary (100-10000):</label>
+                        <input type="number" id="maxSallary" name="tentacles" min="100" max="10000"/>
+                    </div>
+
+                    <div className='centred'>
+                        <div >
+                            <div >Request Status</div>
+                        </div>
+                        <div >
+                            <select>
+                                <option selected value='false'>false</option>
+                                <option value='true'>true</option>
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <div className='centred'>
+                        <label htmlFor="note">Note</label>
+                        <textarea id="note" name="note" rows="2" cols="22" placeholder="some note here">
                     </textarea>
-                </div>
+                    </div>
 
-                <div>
-                    <button>OK</button>
-                    <button onClick={()=>setActive(false)}>CANCEL</button>
-                </div>
-                {children}
+                    <div className='centred btnModal'>
+                        <button type='submit'>OK</button>
+                        <button type='reset' onClick={()=>setActive(false)}>CANCEL</button>
+                    </div>
+                    {children}
+                </form>
+
             </div>
         </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import TableData from "./TableData/TableData.jsx";
 // import AddNewField from "./AddNewField/AddNewField.jsx";
 // import ModalWraper from "./ModalWraper/ModalWraper.jsx";
 import TableHead from "./App/TableHead.jsx";
+import ModalFormic from "./Modal/ModalFormic.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      {/*<ModalFormic/>*/}
       <Header/>
       <TableHead />
       <TableData/>
