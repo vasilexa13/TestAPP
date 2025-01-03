1063a6feaad4f0ed6126b1add42874f5179d9f49
vasilexa13 <vasilexa@gmail.com>
2025-01-03 19:40:22 +0300

    renamed: MONGO_URL in code and .env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoUrl = process.env.MONGODB_URL;
+const mongoUrl = process.env.MONGO_URL;
 
 async function start() {
   try {
